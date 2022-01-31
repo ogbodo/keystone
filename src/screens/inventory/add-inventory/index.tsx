8a8360e6a0024fa9d4a58c7eb5fb8cab65442138
Izuking <matthiasogbodo@gmal.com>
2022-01-31 07:13:03 -0800

    fixes

--- src/screens/inventory/add-inventory/index.tsx
+++ src/screens/inventory/add-inventory/index.tsx
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import {View, Text, Pressable, Image, Alert} from 'react-native';
+import {View, Text, Pressable, Image, Alert, SafeAreaView} from 'react-native';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import styles, {dropDownStyle} from './style';
 import {widthPercentageToDP as WP} from 'react-native-responsive-screen';
@@ -46,155 +46,159 @@ const AddInventory = () => {
   };
 
   return (
-    <KeyboardAwareScrollView keyboardShouldPersistTaps="always">
-      <View style={styles.container}>
-        <View style={styles.Header}>
-          <Pressable
-            onPress={() => {
-              setGlobalData(prev => ({...prev, showAddModal: false}));
-            }}>
-            <Text style={styles.cancel}>Cancel</Text>
-          </Pressable>
-          <Pressable
-            onPress={() => {
-              const {description, id, ...rest} = state;
-              if (inputValidator(rest, true)) {
-                if (canAddValuable(rest.purchasePrice)) {
-                  setGlobalData(prev => ({
-                    ...prev,
-                    valuables: [
-                      ...prev.valuables,
-                      {
-                        ...state,
-                        id: prev.valuables.length + 1,
-                      },
-                    ],
-                    showAddModal: false,
-                  }));
-                } else {
-                  Alert.alert(
-                    'Sorry, you can only add values up to 40,000 euros in total.',
-                  );
+    <SafeAreaView>
+      <KeyboardAwareScrollView keyboardShouldPersistTaps="always">
+        <View style={styles.container}>
+          <View style={styles.Header}>
+            <Pressable
+              onPress={() => {
+                setGlobalData(prev => ({...prev, showAddModal: false}));
+              }}>
+              <Text style={styles.cancel}>Cancel</Text>
+            </Pressable>
+            <Pressable
+              onPress={() => {
+                const {description, id, ...rest} = state;
+                if (inputValidator(rest, true)) {
+                  if (canAddValuable(rest.purchasePrice)) {
+                    setGlobalData(prev => ({
+                      ...prev,
+                      valuables: [
+                        ...prev.valuables,
+                        {
+                          ...state,
+                          id: prev.valuables.length + 1,
+                        },
+                      ],
+                      showAddModal: false,
+                    }));
+                  } else {
+                    Alert.alert(
+                      'Sorry, you can only add values up to 40,000 euros in total.',
+                    );
+                  }
                 }
-              }
-            }}>
-            <Text style={styles.add}>Add</Text>
-          </Pressable>
-        </View>
-        <View style={styles.formCameraContainer}>
-          {state.photo ? (
-            <View style={styles.photoPreviewDeleteContainer}>
-              <View style={styles.photoPreview}>
-                <Image
-                  source={{uri: state.photo, height: WP(40)}}
-                  resizeMode="cover"
-                  height={WP(40)}
-                  width={WP(40)}
+              }}>
+              <Text style={styles.add}>Add</Text>
+            </Pressable>
+          </View>
+          <View style={styles.formCameraContainer}>
+            {state.photo ? (
+              <View style={styles.photoPreviewDeleteContainer}>
+                <View style={styles.photoPreview}>
+                  <Image
+                    source={{uri: state.photo, height: WP(40)}}
+                    resizeMode="cover"
+                    height={WP(40)}
+                    width={WP(40)}
+                  />
+                </View>
+                <MaterialCommunityIcons
+                  name="delete"
+                  size={WP(8)}
+                  color={Colors.PRIMARY}
+                  onPress={() => setState({...state, photo: ''})}
+                  style={[
+                    styles.deleteIcon,
+                    {
+                      paddingLeft: WP(1.2),
+                      paddingTop: WP(0.5),
+                      alignItems: 'center',
+                    },
+                  ]}
                 />
               </View>
-              <MaterialCommunityIcons
-                name="delete"
-                size={WP(8)}
-                color={Colors.PRIMARY}
-                onPress={() => setState({...state, photo: ''})}
-                style={[
-                  styles.deleteIcon,
-                  {
-                    paddingLeft: WP(1.2),
-                    paddingTop: WP(0.5),
-                    alignItems: 'center',
-                  },
-                ]}
-              />
-            </View>
-          ) : (
-            <Pressable
-              style={styles.cameraContainer}
-              onPress={() => {
-                setCanSelectMedia(true);
-              }}>
-              <MaterialCommunityIcons
-                name="camera"
-                size={WP(10)}
-                color={Colors.SECONDARY}
-              />
-              <Text style={styles.cameraLabel}>Add Photo</Text>
-            </Pressable>
-          )}
-          <View style={styles.form}>
-            <View>
-              <Label text="Name" />
-              <Input
-                defaultValue={state.name}
-                placeholder="Bracelet"
-                onChangeText={name => setState({...state, name})}
-                containerStyle={{
-                  marginBottom: WP(2.2),
-                  paddingLeft: WP(2),
-                }}
-              />
-            </View>
-            <View>
-              <Label text="Category" />
-              <ModalDropdown
-                {...dropDownStyle}
-                defaultValue={state.type || 'Select category...'}
-                options={CATEGORIES}
-                style={{...dropDownStyle.style, justifyContent: 'center'}}
-                onSelect={(_, category: string) => {
-                  setState({...state, type: category});
-                }}
-              />
-            </View>
-            <View>
-              <Label text="Value" />
-              <Input
-                defaultValue={`${state.purchasePrice}`}
-                placeholder="700"
-                keyboardType="numeric"
-                onChangeText={purchasePrice =>
-                  setState({...state, purchasePrice: Number(purchasePrice)})
-                }
-                containerStyle={{
-                  marginBottom: WP(2.2),
-                  paddingLeft: WP(2),
-                }}
-                inputFieldIconRight={() => (
-                  <Text style={styles.euro}>{`\u20AC`}</Text>
-                )}
-              />
-            </View>
-            <View>
-              <Label text="Description" />
-              <Input
-                defaultValue={state.description}
-                multiline
-                textInputStyle={{
-                  height: WP(30),
-                  paddingTop: WP(3),
-                  paddingLeft: WP(2),
-                  color: Colors.BLACK_COLOR,
-                }}
-                placeholder="Optional"
-                textAlignVertical="top"
-                onChangeText={description => setState({...state, description})}
-                containerStyle={{
-                  marginBottom: WP(4),
-                  height: WP(30),
-                  paddingTop: WP(3),
-                  paddingLeft: WP(2),
-                }}
-              />
+            ) : (
+              <Pressable
+                style={styles.cameraContainer}
+                onPress={() => {
+                  setCanSelectMedia(true);
+                }}>
+                <MaterialCommunityIcons
+                  name="camera"
+                  size={WP(10)}
+                  color={Colors.SECONDARY}
+                />
+                <Text style={styles.cameraLabel}>Add Photo</Text>
+              </Pressable>
+            )}
+            <View style={styles.form}>
+              <View>
+                <Label text="Name" />
+                <Input
+                  defaultValue={state.name}
+                  placeholder="Bracelet"
+                  onChangeText={name => setState({...state, name})}
+                  containerStyle={{
+                    marginBottom: WP(2.2),
+                    paddingLeft: WP(2),
+                  }}
+                />
+              </View>
+              <View>
+                <Label text="Category" />
+                <ModalDropdown
+                  {...dropDownStyle}
+                  defaultValue={state.type || 'Select category...'}
+                  options={CATEGORIES}
+                  style={{...dropDownStyle.style, justifyContent: 'center'}}
+                  onSelect={(_, category: string) => {
+                    setState({...state, type: category});
+                  }}
+                />
+              </View>
+              <View>
+                <Label text="Value" />
+                <Input
+                  defaultValue={`${state.purchasePrice}`}
+                  placeholder="700"
+                  keyboardType="numeric"
+                  onChangeText={purchasePrice =>
+                    setState({...state, purchasePrice: Number(purchasePrice)})
+                  }
+                  containerStyle={{
+                    marginBottom: WP(2.2),
+                    paddingLeft: WP(2),
+                  }}
+                  inputFieldIconRight={() => (
+                    <Text style={styles.euro}>{`\u20AC`}</Text>
+                  )}
+                />
+              </View>
+              <View>
+                <Label text="Description" />
+                <Input
+                  defaultValue={state.description}
+                  multiline
+                  textInputStyle={{
+                    height: WP(30),
+                    paddingTop: WP(3),
+                    paddingLeft: WP(2),
+                    color: Colors.BLACK_COLOR,
+                  }}
+                  placeholder="Optional"
+                  textAlignVertical="top"
+                  onChangeText={description =>
+                    setState({...state, description})
+                  }
+                  containerStyle={{
+                    marginBottom: WP(4),
+                    height: WP(30),
+                    paddingTop: WP(3),
+                    paddingLeft: WP(2),
+                  }}
+                />
+              </View>
             </View>
           </View>
+          <RenderPickMediaModal
+            isVisible={canSelectMedia}
+            handleMedia={handleMedia}
+            handleBackPress={() => setCanSelectMedia(false)}
+          />
         </View>
-        <RenderPickMediaModal
-          isVisible={canSelectMedia}
-          handleMedia={handleMedia}
-          handleBackPress={() => setCanSelectMedia(false)}
-        />
-      </View>
-    </KeyboardAwareScrollView>
+      </KeyboardAwareScrollView>
+    </SafeAreaView>
   );
 };
 
